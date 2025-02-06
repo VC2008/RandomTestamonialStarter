b1f339b992bbe661ca2c9dc8ae654ee81866d5f5
VC2008 <526Vcolon@frhsd.com>
2025-02-06 11:13:02 -0500

    Arrays and stuff...yeah

---
+++ script.js
@@ -0,0 +1,17 @@
+var quotesArray = [
+    'NAPS is by far the most significant cultural force of the decade. — New York Times',
+'These days sleeping at your desk is not just acceptable, it’s encouraged. — The Atlantic',
+'NAPS has changed the way people think about sleep, on a grand scale. — Washington Post',
+'Happiness increased 200% since NAPS began promoting siestas. — National Science Review',
+'NAPS has ushered in a new era of sleep. — USA Today',
+'Not surprisingly, more siestas means more productivity. — Wall Street Journal',
+'NAPS is doing the hard work of helping people get the rest they need. — U.S. News'
+];
+
+document.getElementById('press-quote').textContent = quotesArray[randomNumber];
+
+Math.random() * 6;
+Math.floor(3.78);
+Math.ceil(3.78);
+
+var randomNumber = Math.floor(Math.random() * quotesArray.length)
